1cb91d7920260b425ba642a898b8050285466c18
Alexander Khramov <alexsandr.khramov@gmail.com>
2024-11-22 21:39:30 +0300

    add sort function

--- complex_struct/complex_struct.cpp
+++ complex_struct/complex_struct.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <functional>
 
 struct complex {
     double Re;
@@ -38,6 +39,24 @@ complex& add(complex& z1, const complex& z2)
     return z1;
 }
 
+double cplx_mod2(const complex& z1){
+    return z1.Re * z1.Re + z1.Im * z1.Im;
+}
+
+int comp_cplx_mod(const complex& z1, const complex& z2) {
+    return cplx_mod2(z1) > cplx_mod2(z2) ? 1 : -1;
+}
+
+void sort(complex* arr, size_t n, std::function<int(const complex&, const complex&)> comp) {
+    for (size_t i = 0; i < n; ++i)
+        for (size_t j = 0; j < n - i - 1; ++j)
+            if (comp(arr[j], arr[j+1]) > 0) {
+                complex temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+}
+
 int main()
 {
     complex z1, z2;
@@ -49,9 +68,20 @@ int main()
     print_cplx(z2);
     std::cout << "\nz1 + z2 = ";
     print_cplx(sum(z1, z2));
-    std::cout << "\nz1 += z2  ";
-    print_cplx(add(z1, z2));
+    std::cout << "\n(z1 += z2) += z2  ";
+    print_cplx(add(add(z1, z2), z2));
     std::cout << "\nz1 = ";
     print_cplx(z1);
     std::cout << "\n";
+
+    complex arr[3] = {};
+    set(arr[0], 1, 0);
+    set(arr[1], 3, 0);
+    set(arr[2], 2, 0);
+
+    sort(arr, 3, comp_cplx_mod);
+    for (int i = 0; i < 3; ++i) {
+        print_cplx(arr[i]);
+        std::cout << "\n";
+    }
 }
